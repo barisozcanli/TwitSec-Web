a8c8529923eb84e1cbbdac76e8bc8ee637c61304
Barış Özcanlı <barisozcanli@gmail.com>
2016-05-13 11:34:22 +0300

    Aded toastr message to profile page

--- app/pages/profile/components/profile.ts
+++ app/pages/profile/components/profile.ts
@@ -3,15 +3,16 @@ import {Router} 		from 'angular2/router';
 import {HTTP_PROVIDERS}	from 'angular2/http';
 import {UserService}	from '../../../shared/services/user.service';
 import {User}           from '../../../shared/models/user';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 
 @Component({
 	selector: 'profile',
 	templateUrl: './pages/profile/components/profile.html',
-  providers: [HTTP_PROVIDERS, UserService]
+  providers: [HTTP_PROVIDERS, UserService, ToastsManager]
 })
 
 export class ProfileCmp {
-  constructor( private _router: Router, private _userService: UserService) {}
+  constructor( private _router: Router, private _userService: UserService, public toastr: ToastsManager) {}
 
   errorMessage: string;
   user: User;
@@ -19,9 +20,16 @@ export class ProfileCmp {
   changeEmail() {
     this._userService.changeEmail(this.user.email).subscribe(
               user => this.user = user,
-              error => this.errorMessage = <any>error
+              error => this.errorMessage = <any>error,
+              () => this.successMsg()
     );
 
+  }
+
+  successMsg() {
+      console.log('toastr start');
+      this.toastr.success('User email updated!', 'Success');
+      console.log('toastr end');
   }
 
   ngOnInit() {
