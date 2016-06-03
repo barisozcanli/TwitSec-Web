4820cc5301bce309329b4b0527723eddf2876a7c
Barış Özcanlı <barisozcanli@gmail.com>
2016-06-03 22:58:48 +0300

    Changed title of the site

--- tools/config.ts
+++ tools/config.ts
@@ -23,7 +23,7 @@ export const HOT_LOADER_PORT      = 5578;
 
 export const BOOTSTRAP_MODULE     = ENABLE_HOT_LOADING ? 'hot_loader_main' : 'main';
 
-export const APP_TITLE            = 'TwitSec';
+export const APP_TITLE            = 'Twitter Secretary : Who Unfollowed Me on Twitter';
 
 export const APP_SRC              = 'app';
 export const ASSETS_SRC           = `${APP_SRC}/assets`;
